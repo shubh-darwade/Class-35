0c0360af4efc2d8c958223f68146dfa3b1d3c33a
shubh-darwade <70751205+shubh-darwade@users.noreply.github.com>
2020-11-26 18:14:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var ball;
+var database,position;
+
+function setup(){
+    createCanvas(500,500);
+
+    database = firebase.database();
+
+    var locofball = database.ref("ball/position");
+
+    locofball.on("value",getposition,showerror);
+
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        putPosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        putPosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        putPosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        putPosition(0,+1);
+    }
+    drawSprites();
+}
+
+function putPosition(x,y){
+
+    database.ref("ball/position").set(
+        {
+           x: position.x +x,
+           y: position.y +y
+        }
+    )
+
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
+
+function getposition(data)
+{
+    position = data.val();
+
+    ball.x=position.x;
+    ball.y=position.y;
+}
+
+function showerror()
+{
+    console.log("error");
+}
